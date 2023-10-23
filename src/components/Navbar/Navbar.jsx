fc2735efadba18227f3ca02a935611df7dbb9f9e
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-10-23 16:23:15 +0600

    Solving logo image problem

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
                             }
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl"><img className="w-16" src="/public/carlogo.png" alt="" /><span>Auto Shop</span></a>
+                    <a className="btn btn-ghost normal-case text-xl"><img className="w-16" src="/carlogo.png" alt="" /><span>Auto Shop</span></a>
                 </div>
 
 
