0a6710aeba1120a70b27ec828db594d78bb02738
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-10-25 00:53:10 +0600

    Designs of Login and Register page updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -61,42 +61,43 @@ const Login = () => {
 
 
     return (
-        <div className="hero min-h-screen bg-base-200 my-9">
-            <div className="hero-content flex flex-col lg:flex-row ">
+        <div className="hero min-h-screen  my-9" >
+            <div className="hero-content w-full md:w-3/4 flex flex-col lg:flex-row " style={{backgroundImage: 'url(/3.png)', borderRadius: '10%', backgroundRepeat: 'no-repeat',  backgroundSize: 'cover'}}>
                 <div className="mr-8"> 
                     <div className="text-center lg:text-left">
                         <h1 className="text-3xl font-bold mb-5">Please login here!</h1>
                         
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl ">
                         <form onSubmit={handleLogin} className="card-body">
                         
                             <div className="form-control">
-                                <label className="label">
-                                <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                                
+                                <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
-                                <label className="label">
-                                <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                                
+                                <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
 
                                 {
-                                    loginError && <p className="text-red-600 pt-4">{loginError}</p>
+                                    loginError &&  
+                                    <div className="bg-base-100 p-4 text-center w-3/4 rounded-lg mx-auto h-20"><p className="text-red-600 pt-4">{loginError}</p></div>
+                                    
+                                   
 
                                 }
                                         
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn btn-accent">Login</button>
                             </div>
 
                            
                         </form>
 
-                        <p className="text-center pb-10">Are you new here? Please <span className="text-blue-700 underline"><Link to="/register">Register</Link></span> </p>
+                        <div className="bg-base-100 p-4 text-center w-3/4 rounded-lg mx-auto h-20">
+                          <p className="text-center font-semibold text-base pb-10">Are you new here? <br /> Please <span className="text-blue-700 underline"><Link to="/register">Register</Link></span> </p>
+                        </div>
                     </div>
                 </div>
                 
@@ -104,10 +105,10 @@ const Login = () => {
 
                <div>
                     <div className="form-control mt-6">
-                            <button onClick={handleGoogleLogin} className="btn btn-outline btn-primary"> <span><FcGoogle className="text-2xl"></FcGoogle></span>Login with Google</button>
+                            <button onClick={handleGoogleLogin} className="btn btn-outline btn-accent"> <span><FcGoogle className="text-3xl"></FcGoogle></span>Login with Google</button>
                     </div>
                     <div className="form-control mt-6">
-                            <button onClick={handleGithubLogin} className="btn btn-outline btn-primary"><span><BsGithub className="text-2xl text-black "></BsGithub></span>Login with GitHub</button>
+                            <button onClick={handleGithubLogin} className="btn btn-outline btn-secondary font-bold "><span><BsGithub className="text-3xl text-black "></BsGithub></span>Login with GitHub</button>
                     </div>
                </div>
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -61,51 +61,49 @@ const Register = () => {
     
 
     return (
-        <div className="hero min-h-screen bg-base-200 my-9">
-            <div className="hero-content flex-col">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-3xl font-bold">Please register here!!</h1>
-                    
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div className="hero min-h-screen  my-14">
+            <div className="hero-content w-full md:w-4/5" style={{backgroundImage: 'url(/4.png)', borderRadius: '10%', backgroundRepeat: 'no-repeat',  backgroundSize: 'cover'}}>
+                
+                <div className="card flex-shrink-0 w-full max-w-sm my-5 shadow-2xl ">
+                <h1 className="text-3xl font-bold p-4">Please register here!!</h1>
                     <form onSubmit={handleRegister} className="card-body">
                         <div className="form-control">
-                            <label className="label">
-                            <span className="label-text">Name</span>
-                            </label>
-                            <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+                           
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
-                            <label className="label">
-                            <span className="label-text">Photo url</span>
-                            </label>
-                            <input type="text" name="photo" placeholder="photo url" className="input input-bordered" />
+                            
+                            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" />
                         </div>
                         <div className="form-control">
-                            <label className="label">
-                            <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                           
+                            <input type="email" name="email" placeholder="E-mail" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
-                            <label className="label">
-                            <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                           
+                            <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
 
                             {
-                                registerError && <p className="text-red-600 pt-4">{registerError}</p>
+                                registerError && 
+                                <div className="bg-base-100 p-4 text-center w-3/4 rounded-lg mx-auto h-20">
+                                     <p className="text-red-600 pt-4 bg-base-100">{registerError}</p>
+                                </div>
+                                
+                               
 
                             }
                             
                             
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Register</button>
+                            <button className="btn btn-accent">Register</button>
                         </div>
                         
                     </form>
-                    <p className="text-center pb-10">Already registered? Please <span className="text-blue-700 underline"><Link to="/login">Login</Link></span> </p>
+                    <div className="bg-base-100 p-4 text-center w-3/4 rounded-lg mx-auto h-20">
+                        <p className="text-center pb-10 font-semibold">Already registered? <br /> Please <span className="text-blue-700 underline"><Link to="/login">Login</Link></span> </p>
+                    </div>
+                    
                 </div>
             </div>
       </div>
